d7bea4c91edb5ceb55947e25b7ea3b7c035322e3
kuomolew <kuomolew@gmail.com>
2019-10-18 00:19:09 +0300

    about us

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("compile", () => {
-  return gulp.src("./css/*.scss")
+  return gulp.src("./css/style.scss")
     .pipe(sass())
     .pipe(gulp.dest((file) => {
       return file.base;
